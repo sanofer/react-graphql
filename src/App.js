8db9f7fd7676b7d94687864ef3d37e0ef175b877
Halima <halimasanofer@gmail.com>
2020-07-03 12:11:08 +0530

    Added graphql dependencies

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import React from 'react'
-
+import ApolloClient from 'apollo-boost'
+import {ApolloProvider} from '@apollo/react-hooks'
+import PokemonsContainer from "./containers/PokemonsContainer.js"
 export default function App(){
-	return(<p>I am pokemon</p>)
-}
+	
+		const client = new ApolloClient({
+			uri:'https://graphql-pokemon.now.sh'
+		});
+		return(
+			<ApolloProvider client={client}>
+            <main>
+                <PokemonsContainer />
+            </main>
+        </ApolloProvider>
+	)
+
+}  
---
+++ src/containers/PokemonsContainer.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function PokemonsContainer(){
+	 return (
+        <p>I am a pokemon container</p>  
+    );
+}
