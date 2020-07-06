f8efd6f6c75bfbe2624dc70b1bc03b228f4d7ad7
Halima <halimasanofer@gmail.com>
2020-07-06 10:17:25 +0530

    Added styles

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ApolloClient from 'apollo-boost'
 import {ApolloProvider} from '@apollo/react-hooks'
 import PokemonsContainer from "./containers/PokemonsContainer.js"
+import './App.css'
 export default function App(){
 	
 		const client = new ApolloClient({
--- src/containers/PokemonsContainer.js
+++ src/containers/PokemonsContainer.js
@@ -1,7 +1,16 @@
-import React from 'react'
+import React from 'react';
+import {useQuery} from 'react-apollo';
+import{GET_POKEMONS} from '../graphql/get-pokemons'
+import Pokemon from '../components/Pokemon'
 
 export default function PokemonsContainer(){
-	 return (
-        <p>I am a pokemon container</p>  
+	 const { data: { pokemons = [] } = {} } = useQuery(GET_POKEMONS, {
+        variables: { first: 9 },
+    });
+    
+    return (
+        <div className="container">
+            {pokemons && pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />)}
+        </div>
     );
 }
